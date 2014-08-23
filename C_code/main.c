175a6a93cfafd85f70b0bfc19795efd5da5857d2
Mark Zang <mark_z@Marks-MacBook-Air.local>
2014-08-22 22:45:24 -0500

    test the estimator using the old C code

---
+++ C_code/main.c
@@ -0,0 +1,40 @@
+#include "AdaptTempering.h"
+#include "AdaptTemperingMultiBin.h"
+#include <stdlib.h>
+
+int main()
+{
+	at_t * at;
+	int index, n = 1000;
+	double Eest;
+  double *varr = NULL;
+	FILE *output = fopen("output.txt", "w");
+	FILE *Eav = fopen("../test_set/Eav.txt", "r");
+	FILE *Efluc = fopen("../test_set/Efluc.txt", "r");
+
+	at = AdaptTempering_MasterCreate("at.cfg", 0, 1, 0);
+  at->mb->flags &= ~MB_CV;
+  at->mb->flags &= ~MB_DAMP;
+
+	/* force change the at->mb->sums */
+	for(index = 0; index <= n; index++) {
+		at->mb->sums[index].s = 1;
+		fscanf(Eav, "%lf", &(at->mb->sums[index].se));
+		fscanf(Efluc, "%lf", &(at->mb->sums[index].se2));
+	}
+	
+	fclose(Eav);
+	fclose(Efluc);
+
+	/* calc the Eest */
+	for(index = 0; index < n; index++){
+		Eest = mb_calc_et(at->mb, index, MB_LOOSE);
+
+		fprintf(output, "%6.3f\n", Eest);		
+	}
+
+	fclose(output);
+
+	AdaptTempering_Close(at);
+}
+
